0e22196a9352817e47aef68e2a2541344397560c
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-18 16:04:36 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+/**
+*the main function here prints "Programming is like building a multilingual puzzle , using the puts function
+*/
 int main(void)
 {
-puts ("Programming is like building a multilingual puzzle\n"); return 0;
+puts(""Programming is like building a multilingual puzzle\n");
+return 0;
 }
