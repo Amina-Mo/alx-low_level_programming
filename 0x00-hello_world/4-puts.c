e96afd8f6ff82948f0f210362936fdf5ff5a5fc8
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-18 13:27:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,1 @@
-#!/bin/bash
-#include <stdio>; int main(void) { puts ("Programming is like building a multilingual puzzle\n"); return (0); }
+#include <stdio> ; int main(void) { puts ("Programming is like building a multilingual puzzle\n"); return (0); }
