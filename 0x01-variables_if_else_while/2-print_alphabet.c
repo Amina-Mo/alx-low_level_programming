fee40403d57a87d7974058d3ada64b57c0efde48
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-20 15:15:45 -0700

    print low case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*prints all the alphabet using function putchar
+*/
+int main(void)
+{
+char a;
+a=a;
+if ( a <= z )
+{
+putchar (a);
+a++;
+}
+else
+{
+return 0;
+}
+return 0;
+}
