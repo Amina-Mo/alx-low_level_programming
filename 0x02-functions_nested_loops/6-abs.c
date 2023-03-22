ac30a7530ac7c64bbed34d95b07de9c0aec806b0
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-22 08:26:45 -0700

    absolut int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of an integer
+ *
+ * Return: void
+ */
+int _abs(int)
+{
+	int a;
+	int abs;
+
+	if (a < 0)
+	{
+		abs = -a;
+	}
+	else
+	{
+		abs = a;
+	}
+	return (abs);
+}
+
