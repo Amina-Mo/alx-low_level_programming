517a65838148203453d8628d7c93b19e9817aa7d
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-22 09:05:48 -0700

    jack bauer day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of Jack Bauer day
+ * 
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar(hour + '0');
+			_putchar(':');
+			_putchar(minute + '0');
+		}
+	}
+}
+
