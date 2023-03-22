7d8b145f90da229b01ad21f19bd254a44c04a904
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-22 10:24:27 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int line, colon, n;
+
+	for (colon = 0; colon < 10; colon++)
+	{
+		for (line = 0; line < 10; line++)
+		{
+			n = line * colon;
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar('$');
+		}
+		_putchar('\n');
+	}
+}
+
