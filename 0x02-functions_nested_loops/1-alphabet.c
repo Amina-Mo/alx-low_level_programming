61189e2fd60ca173f0e57f8aaaf31a5f228ec5f3
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-22 04:41:50 -0700

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all the alphabets in lowercase
+ *
+ * Return: on seccess 0
+ * on error any other number
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	return (0);
+}
+
