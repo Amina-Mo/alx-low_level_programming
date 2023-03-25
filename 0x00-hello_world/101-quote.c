bfbb35ce6587353bfbb8552a4b0f3d00579315d1
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-25 13:23:22 -0700

    putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints a statement
  *
@@ -6,66 +6,9 @@
  */
 int main(void)
 {
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar(' ');
-	putchar('"');
-	putchar(' ');
-	putchar('-');
-	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	putchar('2');
-	putchar('0');
-	putchar('1');
-	putchar('5');
-	putchar('-');
-	putchar('1');
-	putchar('0');
-	putchar('-');
-	putchar('1');
-	putchar('9');
-	putchar('\n');
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(STDERR_FILENO, message, 40);
 	return (1);
 }
 
