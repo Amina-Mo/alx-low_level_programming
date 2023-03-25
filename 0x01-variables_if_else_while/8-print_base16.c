373c96b1e92bd610cf83d74e0e2bdeaa316bf69f
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-25 16:35:04 -0700

    hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	char hex[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	char hexa[6] = {'a', 'b', 'c', 'd', 'e', 'f'};
 
-	for (i = 0; i < 10; i++) 
+	for (i = 0; i < 10; i++)
 	{
 		putchar(hex[i]);
 	}
