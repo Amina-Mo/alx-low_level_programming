08936fd93c2288d47b40aad4504944a5df8fd2e3
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-25 15:37:00 -0700

    ouff

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets execpt q and e
+ *
+ * Return: on success 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+}
+
