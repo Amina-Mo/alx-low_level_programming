313065a07547448f51be135d6f1aa06559ec5d38
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-26 08:38:10 -0700

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,15 +7,16 @@
 int main(void)
 {
 	int a = 1, b = 2;
+	int i;
 	int fibonacci_numbers[50];
 	
 	fibonacci_numbers[0] = a;
 	fibonacci_numbers[1] = b;
-	for (int i = 2; i < 50; i++)
+	for (i = 2; i < 50; i++)
 	{
 		fibonacci_numbers[i] = fibonacci_numbers[i-1] + fibonacci_numbers[i-2];
 	}
-	for (int i = 0; i < 49; i++)
+	for (i = 0; i < 49; i++)
 	{
 		printf("%d, ", fibonacci_numbers[i]);
 	}
