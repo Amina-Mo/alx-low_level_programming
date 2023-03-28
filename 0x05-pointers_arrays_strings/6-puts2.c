0361245cc92e5176b9e398df7c831584a91b126f
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-28 15:48:37 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints every other character
+ * @str: thestring to print
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 = 0)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
+
