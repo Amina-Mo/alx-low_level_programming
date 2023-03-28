6b76983b4e08da605fbfe1e5574067a520535c37
Amina Bouslim <amina.bouslim@gmail.com>
2023-03-28 08:03:58 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int tmp = s;
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+	while (s >= tmp)
+	{
+		_putchar(*s);
+		s--;
+	}
+}
+
