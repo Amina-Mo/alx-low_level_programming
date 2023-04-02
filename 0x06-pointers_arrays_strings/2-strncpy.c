5f09d8e6dec5cedf787d15fa3f4ac356414d6f55
Amina Bouslim <amina.bouslim@gmail.com>
2023-04-02 10:55:51 -0700

    string n copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: int
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		*ptr++ = *src++;
+	}
+	for (i < n)
+	{
+		ptr[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
