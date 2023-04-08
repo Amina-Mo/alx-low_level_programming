32f00007eb3f54536f7e96b73e9a559801145ab4
Amina Bouslim <amina.bouslim@gmail.com>
2023-04-08 11:33:08 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strspn - num of bytes
+ * @s: initial string
+ * @accept: string
+ * Return: number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int num = 0;
+
+	while (*accept != '\0')
+	{
+		while (*s != '\0')
+		{
+			if (*s == *accept)
+			{
+				num = num + 1;
+			}
+			s++;
+		}
+		accept++;
+	}
+	return (sum);
+}
+
