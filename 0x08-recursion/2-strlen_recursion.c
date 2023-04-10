1539a3472f496af456d16a4cfc87d1f330da7601
Amina Bouslim <amina.bouslim@gmail.com>
2023-04-10 15:05:43 -0700

    string length recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	if (*s != '\0')
+	if (*s)
 	{
-		_strlen_recursion(s + 1);
-		i++;
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return (i);
 }
 
