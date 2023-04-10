f352ae135b2556f244164bcefd76de44d24d5592
Amina Bouslim <amina.bouslim@gmail.com>
2023-04-10 15:25:37 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - factorial n
+ * @n: number
+ * Return: result
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else if (n > 1)
+	{
+		return (n * factorial(n - 1));
+	}
+}
+
