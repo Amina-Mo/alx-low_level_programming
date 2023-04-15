2c4a93e11d5b11067fe84601936614fc3afb3c86
Amina Bouslim <amina.bouslim@gmail.com>
2023-04-15 15:50:59 -0700

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints its name
+ * Return: void
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
