5bb20c9c664a17da82915265969efceb9371f6b1
Amina Bouslim <amina.bouslim@gmail.com>
2023-04-16 07:12:10 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,13 +17,23 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	p = malloc(width * height * sizeof(int));
+	p = malloc(height * sizeof(int *));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
 	{
+		p[i] = malloc(width * sizeof(int));
+		if (p[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(p[j]);
+			}
+			free(p);
+			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			p[i][j] = 0;
