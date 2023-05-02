cae81d5d6e073db3bbfb78066d46eb837db134e3
Amina Bouslim <amina.bouslim@gmail.com>
2023-05-02 13:39:43 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * dog - id
+ * struct dog - id
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Decription: dog id
  */
-
 struct dog {
 	char *name;
 	float age;
